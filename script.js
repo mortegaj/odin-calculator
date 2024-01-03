bd5d955fbbc85e46e05efcb69c98e24390bd4246
mortegaj <marcmordigan@gmail.com>
2024-01-03 22:11:36 +0100

    Create basic math operations functions

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!!");
+function add (a,b) {
+    return a + b;
+}
+
+function substract (a,b) {
+    return a - b;
+}
+
+function multiply (a,b) {
+    return a * b;
+}
+
+function divide (a,b) {
+    return a / b;
+}
+
+console.log ("3 + 2 = " + add(3,2));
+console.log ("3 - 2 = " + substract(3,2));
+console.log ("3 * 2 = " + multiply(3,2));
+console.log ("5 / 2 = " + divide(5,2));
+console.log ("5 + 0 = " + divide(3,0));
