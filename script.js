c19b66fe04be60adb47f4051a7f9a80b9c396859
mortegaj <marcmordigan@gmail.com>
2024-01-04 20:24:20 +0100

    Create operate function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 let op1;
 let op2;
 let operator;
@@ -19,35 +20,11 @@ function divide (a,b) {
     else return "div/0";
 }
 
-// Test 1
-op1 = 3;
-op2 = 2;
-operator = add;
-console.log(`${op1} + ${op2} = ${operator(op1,op2)}`);
-
-// Test 2
-op1 = 3;
-op2 = 2;
-operator = substract;
-console.log(`${op1} - ${op2} = ${operator(op1,op2)}`);
-
-// Test 3
-op1 = 3;
-op2 = 2;
-operator = multiply;
-console.log(`${op1} * ${op2} = ${operator(op1,op2)}`);
-
-// Test 4
-op1 = 5;
-op2 = 2;
-operator = divide;
-console.log(`${op1} / ${op2} = ${operator(op1,op2)}`);
-
-
-// Test 5
-op1 = 5;
-op2 = 0;
-operator = divide;
-console.log(`${op1} / ${op2} = ` + operator(op1,op2));
-
-
+function operate () {
+    switch (operator) {
+        case "+": return add(op1,op2);
+        case "-": return substract(op1,op2);
+        case "*": return multiply(op1,op2);
+        case "/": return divide(op1,op2);
+    }
+}
