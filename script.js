1174e2f974d2b5b5e10215e20a771370e4b77bfd
mortegaj <marcmordigan@gmail.com>
2024-01-23 22:37:07 +0100

    Capture new operand

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Hello World!!");
+let newOperand = null;
+
+function captureOperand (value) {
+    if (newOperand === null) newOperand = value;
+    else newOperand += value;
+}
+
+let exit_condition = false;
+while (!exit_condition) {
+    input = prompt("Press new key");
+    if (input == "q") exit_condition = true;
+    else {
+        captureOperand(input);
+        console.log(newOperand);
+    }
+}
