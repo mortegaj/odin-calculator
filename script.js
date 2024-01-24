cf707f04201d740d5c672b10bb4cd92d81637af1
mortegaj <marcmordigan@gmail.com>
2024-01-24 22:24:16 +0100

    Add keyPressed function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let first_operand = null;
 let second_operand = null;
+let operator = null;
 
 function setNewOperand (operand,value) {
     if (operand === null) operand = value;
@@ -7,12 +8,22 @@ function setNewOperand (operand,value) {
     return operand;
 }
 
+function keyPressed (key) {
+    if (operator == null) {
+        //Entering first operand
+        first_operand = setNewOperand(first_operand,key);
+    } else {
+        //Entering second operand
+        second_operand = setNewOperand(second_operand,key);
+    }
+}
+
 let exit_condition = false;
 while (!exit_condition) {
     input = prompt("Press new key");
     if (input == "q") exit_condition = true;
     else {
-        first_operand = setNewOperand(first_operand,input);
-        console.log(first_operand);
+        keyPressed (input);
+        console.log(`first-operand:${first_operand} | operator:${operator} | second-operand:${second_operand}`);
     }
 }
